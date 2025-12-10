5b5c2ff60deb3e1daffab051d088660f2af4868f
Jhon78902 <mdsuzanmia78902@gmail.com>
2025-12-10 14:24:45 +0900

    Add Day 11 exercises: dictionaries and strings

---
+++ Day11_Exercises/day11_exercises.py
@@ -0,0 +1,11 @@
+# Dictionary of grades
+grades = {"Math":92, "English":90, "History":88}
+# Print each subject and grade
+for subject, grade in grades.items():
+    print(f"Subject: {subject}, Grade: {grade}")
+# String input and formatting
+name = input("Enter your name:")
+print(f"Hello {name}, welcome to Day 11 excercise")
+# Student dictonary
+student = {"Name": "Suzanmia", "Course": "Computer Science", "Goal": "Engineer"}
+print(f"{student['Name']} is studying {student['Course']}and wants to be an {student['Goal']}.")
